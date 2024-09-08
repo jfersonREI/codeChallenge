02d2242be4a47896b5fe96f9296423b1dd211952
jfersonREI <jeff.ferson@reisystems.com>
2024-09-08 18:21:13 -0400

    init

--- app/src/components/AppMain/AppMain.js
+++ app/src/components/AppMain/AppMain.js
@@ -1,11 +1,14 @@
 import AppPanel from "../AppPanel/AppPanel";
+import AppPanelGrid from "../AppPanelGrid/AppPanelGrid";
 import "./AppMain.scss";
 
 function AppMain(props) {
   return (
     <div className="app-main">
       <div className="grid-container">
-        <AppPanel>{props.children}</AppPanel>
+        <AppPanel>
+          <AppPanelGrid>{props.children}</AppPanelGrid>
+        </AppPanel>
       </div>
     </div>
   );
---
+++ app/src/components/AppPanelGrid/AppPanelGrid.js
@@ -0,0 +1,7 @@
+import "./AppPanelGrid.scss";
+
+function AppPanelGrid(props) {
+  return <div className="app-panel-grid"> {props.children}</div>;
+}
+
+export default AppPanelGrid;
