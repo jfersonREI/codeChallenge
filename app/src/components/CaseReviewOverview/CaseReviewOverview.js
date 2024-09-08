a9bff472de09739d4c0e6cd0adb66a3bbdb64c3f
jfersonREI <jeff.ferson@reisystems.com>
2024-09-08 14:48:24 -0400

    init

--- app/src/components/CaseReviewOverview/CaseReviewOverview.js
+++ app/src/components/CaseReviewOverview/CaseReviewOverview.js
@@ -1,7 +1,13 @@
 import "./CaseReviewOverview.scss";
 
 function CaseReviewOverview() {
-  return <div className="case-review-overview">ccc</div>;
+  return (
+    <div className="case-review-overview">
+      <div className="case-review-overview__col">col1</div>
+      <div className="case-review-overview__col">col2</div>
+      <div className="case-review-overview__col">col3</div>
+    </div>
+  );
 }
 
 export default CaseReviewOverview;
