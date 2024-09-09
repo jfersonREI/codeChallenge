ff596c14dfbdcef1d4d0ef52e9dc576f314afee6
jfersonREI <jeff.ferson@reisystems.com>
2024-09-09 00:07:41 -0400

    init

--- app/src/components/Divider/Divider.js
+++ app/src/components/Divider/Divider.js
@@ -1,6 +1,9 @@
+import "./Divider.scss";
+
 function Divider({ color = "black", height = 1 }) {
   return (
     <hr
+      className="divider"
       style={{
         backgroundColor: color,
         height: height,
